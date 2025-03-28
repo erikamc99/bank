3ede55bd1dbf4da332248cf13db28d220056bd7a
Erica <you@example.com>
2025-03-28 11:33:11 +0100

    feat: test Account created. initialBalanceTest done. TDD: ❌

---
+++ src/test/java/dev/erica/bank/model/AccountTest.java
@@ -0,0 +1,28 @@
+package dev.erica.bank.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+    
+    public static class TestAccount extends Account {
+        protected float balance;
+
+        public TestAccount(float initialBalance){
+            super(initialBalance);
+        }
+    }
+
+    private TestAccount account;
+
+    @Test
+    @DisplayName("Test para comprobar el atributo de saldo inicial")
+    public void initialBalanceTest() {
+
+        account = new TestAccount(100);
+
+        assertEquals(100, account.getBalance());
+    }
+}
