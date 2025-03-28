59f4b9af29d2e1078bf6ba2c9f4bdba7b44f6f76
Erica <you@example.com>
2025-03-28 11:44:52 +0100

    feat: annual rate implemented. TDD: ✔️

--- src/main/java/dev/erica/bank/model/Account.java
+++ src/main/java/dev/erica/bank/model/Account.java
@@ -2,10 +2,13 @@ package dev.erica.bank.model;
 
 public abstract class Account {
     protected float balance;
+    protected float annualRate;
 
-    public Account(float initialBalance) {
+    public Account(float initialBalance, float annualRate) {
         this.balance = initialBalance;
+        this.annualRate = annualRate;
     }
 
     public float getBalance() { return balance; }
+    public float getAnnualRate() { return annualRate; }
 }
