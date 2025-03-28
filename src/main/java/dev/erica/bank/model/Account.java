2e1e13db586d8b8461e2b7326c681072afdf75fb
Erica <you@example.com>
2025-03-28 11:53:04 +0100

    feat: totalDeposits implemented. TDD: ✔️

--- src/main/java/dev/erica/bank/model/Account.java
+++ src/main/java/dev/erica/bank/model/Account.java
@@ -3,12 +3,15 @@ package dev.erica.bank.model;
 public abstract class Account {
     protected float balance;
     protected float annualRate;
+    protected int totalDeposits;
 
     public Account(float initialBalance, float annualRate) {
         this.balance = initialBalance;
         this.annualRate = annualRate;
+        this.totalDeposits = 0;
     }
 
     public float getBalance() { return balance; }
     public float getAnnualRate() { return annualRate; }
+    public int getTotalDeposits() { return totalDeposits; }
 }
