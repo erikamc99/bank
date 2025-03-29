639fde559d2203aaac44f761df908c7cfcce7e40
Erica <you@example.com>
2025-03-29 20:22:46 +0100

    feat: refactor tests with a beforeEach and add getOverdraft at depositTest to full coverage. Deposit without overdraft done. TDD: ✔️

--- src/test/java/dev/erica/bank/model/CheckingAccountTest.java
+++ src/test/java/dev/erica/bank/model/CheckingAccountTest.java
@@ -1,26 +1,41 @@
 package dev.erica.bank.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CheckingAccountTest {
 
     private CheckingAccount account;
+    private CheckingAccount accountO;
+
+    @BeforeEach
+    void setUp() {
+        account = new CheckingAccount(5000, 0.05f, 0);
+        accountO = new CheckingAccount(5000, 0.05f, 1000);
+    }
 
     @Test
     @DisplayName("Test para comprobar el sobregiro")
     public void overdraftTest() {
-        account = new CheckingAccount(5000, 0.05f, 1000);
-        assertEquals(1000, account.getOverdraft());
+        accountO.getOverdraft();
+        assertEquals(1000, accountO.getOverdraft());
     }
 
     @Test
-    @DisplayName("Test para comprobar el ingreso de dinero")
+    @DisplayName("Test para comprobar el ingreso de dinero con sobregiro")
     public void depositTest() {
-        account = new CheckingAccount(5000, 0.05f, 1000);
+        accountO.deposit(5000);
+
+        assertEquals(9000, accountO.getBalance());
+    }
+
+    @Test
+    @DisplayName("Test para comprobar el ingreso de dinero sin sobregiro")
+    public void depositWithoutOverdraftTest() {
         account.deposit(5000);
 
-        assertEquals(9000, account.getBalance());
+        assertEquals(10000, account.getBalance());
     }
-}
+} 
